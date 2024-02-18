edb969fb69f08ff5022833ba65c02381888e21c8
TENSAEA <tensaeaschalew26@gmail.com>
2024-02-18 21:56:52 +0300

    update app.js

--- App.js
+++ App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [count, setCount] = useState(0);
+  const [celceius, setCelceius] = useState(0);
+  const [fahrenheit, setFahrenheit] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+
+      {count === 0 && <h1>{count} is neutral</h1>}
+      {count > 0 && <h1>{count} is positive</h1>}
+      {count < 0 && <h1>{count} is negative</h1>}
+      {<hr></hr>}
+      {<br></br>}
+      <input
+        type="number"
+        value={celceius}
+        onChange={(e) => {
+          setCelceius(e.target.value);
+        }}
+      ></input>
+      <input
+        type="number"
+        value={fahrenheit}
+        onChange={(e) => {
+          setFahrenheit(e.target.value);
+        }}
+      ></input>
+      {celceius && <h1>{celceius}°C</h1>}
+      {fahrenheit && <h1>{fahrenheit}°F</h1>}
     </div>
   );
 }
